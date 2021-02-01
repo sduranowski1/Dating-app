1329e0db6c138dde4f6e4f05030d37e48befa03b
Simon Duranowski <sduranowski1@gmail.com>
2021-02-01 14:13:07 +0100

    Update TinderCards.js

--- TinderCards.js
+++ TinderCards.js
@@ -1,57 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import TinderCard from "react-tinder-card";
-import database from './firebase'
-import './TinderCards.css';
+import './SwipingCards.css';
 
 
 
-function TinderCards() {
+function SwipingCards() {
     const [people, setPeople] = useState([
         {
-            name: 'Kacper, 13',
+            name: 'Kacper, 18',
             url: 'https://allaboutstevejobs.com/resources/img/pics/1976-01-01_00030.jpg'
         },
         {
-            name: 'Pjotr, 21',
+            name: 'Piotr, 21',
             url: 'https://i.insider.com/52f3fdaeeab8ea35176ea827?width=750&format=jpeg&auto=webp'
         }
     ]);
-    // const people = [];
 
-    useEffect(() => {
-        const unsubscribe = database
-            .collection('people')
-            .onSnapshot((snapshot) => setPeople(snapshot.docs.map((doc) => doc.data()))
-            );
-        return () => {
-            unsubscribe();
-        };
-    }, []);
+
 
     return (
         <div>
 
 
-            <div className="tinderCards__cardContainer">
+            <div className="swipingCards__cardContainer">
 
             
                 {people.map((person) => (
-                    <TinderCard
+                    <SwipingCard
                         className="swipe"
                         key={person.name}
                         preventSwipe={['up', 'down']}
-                    >
+                     >
                         <div 
                             style={{ backgroundImage: `url(${person.url})`}}
                             className="card"
                         >
                             <h1>{person.name}</h1>
                         </div>
-                    </TinderCard>
+                    </SwipingCard>
                 ))}
             </div>
         </div>
     );
 }
 
-export default TinderCards
+export default SwipingCards
